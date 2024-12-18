4710b96be934e3d8eb639c726f2c40336dc76741
vitali <vitali.averkin@stud.hs-kempten.de>
2024-12-18 21:30:06 +0100

    Initialize clean gh-pages branch with index.html

--- deintraumgarten/myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import TodoItem
-
-# Register your models here.
-# Nachdem models registriert wurden oder datenbank models verändert wurden, 
-# muss im terminal folgendes ausgeführt werden: python manage.py makemigrations,
-# danach python manage.py migrate
-
-admin.site.register(TodoItem)
